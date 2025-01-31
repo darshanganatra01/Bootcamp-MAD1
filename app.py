007a27dd7eeda3dcc9468f86236ae475fcbb9373
darshanganatra01 <darshanganatra28@gmail.com>
2025-01-31 21:04:08 +0530

    CRUD

--- app.py
+++ app.py
@@ -53,17 +53,110 @@ def login():
     if request.method=='GET':
         return render_template("login.html")
     if request.method=='POST':
-        first_name=request.form.get('Fname')
-        last_name=request.form.get('Lname')
+        first_name=request.form.get('name')
+        password=request.form.get('password')
         print(first_name)
-        print(last_name)
-        return redirect('/dashboard/'+first_name)
+        userexist = User.query.filter_by(name=first_name).first()
+        
+        #then will check the password
+        if userexist is None:
+            print("user doesnt exist")
+            return redirect('/register')
+        
+        else:
+            if userexist.password==password:
+                return redirect('/dashboard/'+str(userexist.id))
+            else:
+                return "password doesnt match"
+            #we will learn how to use flash msgs
     
-@app.route('/dashboard/<first_name>',methods=['GET','POST'])
-def dashboard(first_name):
+    
+    
+@app.route('/dashboard/<id>',methods=['GET','POST'])
+def dashboard(id):
     if request.method=='GET':
-        return render_template("Dashboard.html",first_name=first_name)
+        userdetails=User.query.filter_by(id=int(id)).first()
+        return render_template("Dashboard.html",userdetails=userdetails)
     
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='GET':
+        return render_template('register.html')
+    else:
+        name = request.form.get('name')
+        password = request.form.get('password')
+
+        #creating and instance 
+        print(name,password)
+
+
+        new_user = User(name=name,password=password)
+        #L.H.S is what name is the name in the database
+        #R.H.S the name you want to give
+
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect('/login')
+    
+    
+@app.route('/all_users',methods=['GET'])
+def all_users():
+    users = User.query.all()  #list of all objects of the table(Every entry is an object of user class) 
+    print(users)
+
+    userfirst=User.query.filter_by(name='Darshan').first()
+    #give me the first user whose name is darshan
+
+    userall=User.query.filter_by(name="Darshan").all()
+    #give me all the users whose name is darshan 
+
+    return render_template("all_users.html",userfirst=userfirst,userall=userall)
+
+
+
+@app.route('/update/<id>',methods=['GET','POST'])
+def update(id):
+    if request.method=='GET':
+        return render_template("update.html")
+    else:
+        new_name=request.form.get('name')
+        new_password=request.form.get('password')
+
+    userthatiwanttoupdate = User.query.filter_by(id=id).first()
+
+    userthatiwanttoupdate.name = new_name
+    userthatiwanttoupdate.password = new_password
+
+    db.session.commit()
+    return redirect('/dashboard/'+str(userthatiwanttoupdate.id))
+
+
+
+
+
+@app.route('/delete/<id>')
+def delete(id):
+    userthatiwanttodelete =  User.query.filter_by(id=id).first()
+    db.session.delete(userthatiwanttodelete)
+    db.session.commit()
+    return "Deleted the user successfullty"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
---
+++ application/controllers/routes.py
@@ -0,0 +1,46 @@
+from flask import Blueprint,Flask,render_template,request,redirect,url_for
+from application.models.model import db
+
+bp = Blueprint('main', __name__)
+
+
+@bp.route('/')
+def default():
+    return render_template("landing.html")
+    
+
+@bp.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        return render_template("login.html")
+    if request.method=='POST':
+        first_name=request.form.get('Fname')
+        last_name=request.form.get('Lname')
+        print(first_name)
+        print(last_name)
+        return redirect('/dashboard/'+first_name)
+    
+@bp.route('/dashboard/<first_name>',methods=['GET','POST'])
+def dashboard(first_name):
+    if request.method=='GET':
+        return render_template("Dashboard.html",first_name=first_name)
+    
+
+@bp.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='GET':
+        return render_template("register.html")
+    else:
+        name = request.form.get('name')
+        password = request.form.get("password")
+        print(name,password)
+        new_user=User()
+
+
+
+
+    
+
+    
+
+#methods are the list of methods only allowed methods to the server
